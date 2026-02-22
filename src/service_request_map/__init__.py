eddc83f9638ffd5afb07b3a4b341bddb1d3414ac
Tara Sullivan <tsullivan126@gmail.com>
2026-02-22 09:48:54 -0500

    initial uv setup

---
+++ src/service_request_map/__init__.py
@@ -0,0 +1,4 @@
+# src/service_request_map/__init__.py
+from service_request_map.main import main
+
+__all__ = ['main']
---
+++ src/service_request_map/main.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from service-request-map!")
