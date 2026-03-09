5b8a1c0d77ce03f9f6d2cce5eaebfc14bd00937f
Tara Sullivan <tsullivan126@gmail.com>
2026-03-09 15:16:18 -0400

    HTML map (not folium)

--- notebooks/sr_heat.py
+++ notebooks/sr_heat.py
@@ -17,14 +17,10 @@ def _(mo):
 def _():
     import pandas as pd
     import marimo as mo
-
     import geopandas as gpd
-    import folium
-    import branca.colormap as cm
+    import pathlib
 
-    # from service_request_map.extract.get_sr_socrata import get_sr_socrata
-    # from service_request_map.extract.get_zip_geojson import get_zip_geojson
-    return cm, folium, gpd, mo, pd
+    return gpd, mo, pathlib, pd
 
 
 @app.cell
@@ -38,9 +34,10 @@ def _(mo):
 
 
 @app.cell
-def _(gpd, pd):
-    sr_df = pd.read_parquet('data/sr_data.parquet')
-    zip_gdf = gpd.read_parquet('data/zip_geo.parquet')
+def _(gpd, mo, pathlib, pd):
+    _base = pathlib.Path(mo.notebook_location()).parents[0]
+    sr_df = pd.read_parquet(_base / 'data' / 'sr_data.parquet')
+    zip_gdf = gpd.read_parquet(_base / 'data' / 'zip_geo.parquet')
     return sr_df, zip_gdf
 
 
@@ -72,18 +69,14 @@ def _(mo, sr_df):
 def _(complaint_dropdown, end_date, mo, pd, sr_df, start_date):
     group_by_filter = pd.Series(index=sr_df.index, data=True)
 
-    # complaint_filter= pd.Series(index=sr_df.index, data=True)
-
-    # date_filter = pd.Series(index=sr_df.index, data=True)
-
     if complaint_dropdown.value is not None:
         complaint_filter = (sr_df['complaint_type'] == complaint_dropdown.value)
     else:
-        complaint_filter= pd.Series(index=sr_df.index, data=True)
+        complaint_filter = pd.Series(index=sr_df.index, data=True)
 
     date_filter = (
-            (sr_df['created_date_dt'] >= start_date.value) &
-            (sr_df['created_date_dt'] < (end_date.value + pd.Timedelta(1, 'D')))
+        (sr_df['created_date_dt'] >= start_date.value) &
+        (sr_df['created_date_dt'] < (end_date.value + pd.Timedelta(1, 'D')))
     )
 
     filter_df = sr_df.loc[(group_by_filter & complaint_filter & date_filter), ['unique_key', 'incident_zip']]
@@ -108,7 +101,7 @@ def _(complaint_dropdown, end_date, mo, pd, sr_df, start_date):
 def _(group_by_df, pd, zip_gdf):
     zip_count_gdf = pd.merge(
         left=zip_gdf[['GEOID', 'geometry']].rename(columns={'GEOID': 'zip'}),
-        right=group_by_df, 
+        right=group_by_df,
         on='zip',
         how='left', validate='1:1'
     )
@@ -117,37 +110,69 @@ def _(group_by_df, pd, zip_gdf):
 
 
 @app.cell
-def _(cm, folium, zip_count_gdf):
-    map = folium.Map(
-        location=[40.70, -73.94],
-        zoom_start=10,
-        tiles="CartoDB positron"
-    )
+def _(mo, zip_count_gdf):
+    import json
 
-    colormap = cm.linear.YlOrRd_09.scale(
-        zip_count_gdf['count'].min(),
-        zip_count_gdf['count'].max()
-    )
+    _geojson_str = zip_count_gdf[['zip', 'count', 'geometry']].to_json()
 
-    folium.GeoJson(
-        zip_count_gdf,
-        style_function=lambda feature: {
-            "fillColor": colormap(feature["properties"]["count"]),
-            "color": "black",
-            "weight": 0.5,
-            "fillOpacity": 0.7,
-        },
-        tooltip=folium.GeoJsonTooltip(
-            fields=["zip", "count"],
-            aliases=["ZIP Code:", "Count:"],
-            localize=True,
-            sticky=False
-        )
-    ).add_to(map)
+    mo.iframe(f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+      <style> body {{ margin: 0; }} #map {{ height: 600px; width: 100%; }} </style>
+    </head>
+    <body>
+    <div id="map"></div>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script>
+      var map = L.map('map').setView([40.70, -73.94], 10);
+      L.tileLayer('https://{{s}}.basemaps.cartocdn.com/light_all/{{z}}/{{x}}/{{y}}{{r}}.png', {{
+        attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
+      }}).addTo(map);
 
-    colormap.add_to(map)
+      var data = {_geojson_str};
+      var counts = data.features.map(f => f.properties.count);
+      var maxCount = Math.max(...counts);
 
-    map
+      function getColor(val) {{
+        if (maxCount === 0) return '#ffffcc';
+        var t = val / maxCount;
+        var stops = [
+          [255, 255, 204],
+          [254, 217, 118],
+          [253, 141,  60],
+          [227,  26,  28],
+          [128,   0,  38],
+        ];
+        var i = Math.min(Math.floor(t * (stops.length - 1)), stops.length - 2);
+        var f = t * (stops.length - 1) - i;
+        var r = Math.round(stops[i][0] + f * (stops[i+1][0] - stops[i][0]));
+        var g = Math.round(stops[i][1] + f * (stops[i+1][1] - stops[i][1]));
+        var b = Math.round(stops[i][2] + f * (stops[i+1][2] - stops[i][2]));
+        return 'rgb(' + r + ',' + g + ',' + b + ')';
+      }}
+
+      L.geoJSON(data, {{
+        style: function(feature) {{
+          return {{
+            fillColor: getColor(feature.properties.count),
+            color: 'black',
+            weight: 0.5,
+            fillOpacity: 0.7
+          }};
+        }},
+        onEachFeature: function(feature, layer) {{
+          layer.bindTooltip(
+            '<b>ZIP:</b> ' + feature.properties.zip +
+            '<br><b>Count:</b> ' + feature.properties.count
+          );
+        }}
+      }}).addTo(map);
+    </script>
+    </body>
+    </html>
+    """, height=620)
     return
 
 
